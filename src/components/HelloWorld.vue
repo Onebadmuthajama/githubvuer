428da2fbaccdba23f480cd0f764b570086eefa15
onebadmuthajama <kadenlovell@live.com>
2020-03-10 00:03:20 -0600

    reverted code back to latest stable for recursive refactor

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <v-btn
       v-if="loaded"
       v-on:click="
-        getRepoFiles();
+        getRepositoryRoot();
         loaded = !loaded;
       "
       >Load Repository</v-btn
@@ -19,29 +19,16 @@
     </v-btn>
     <v-container class="d-flex">
       <v-treeview
-        v-if="repository != null"
-        :items="testObject"
+        v-if="ready"
+        :items="repository[0].children"
         hoverable
         class="col-4"
-        item-key="name"
+        item-key="children"
       >
         <template slot="label" slot-scope="{ item }">
-          <a @click="loadFile(`${item.name}`, `${item.parent}`)">{{
-            item.name
-          }}</a>
+          <a @click="itemClick(item.name)">{{ item.name }}</a>
         </template>
       </v-treeview>
-
-      <v-card v-if="filecontent != null" class="col-8">
-        <v-card-text>
-          <p class="display-1 text--primary">
-            {{ filename }}
-          </p>
-          <div class="text--primary">
-            <p>{{ filecontent }}}}</p>
-          </div>
-        </v-card-text>
-      </v-card>
     </v-container>
   </v-container>
 </template>
@@ -54,35 +41,30 @@ export default {
   data: () => ({
     key1: "6b4282865d685",
     key2: "e4889714aa9cb982ac9710e2a18",
-    repository: null,
-    subdir: [
-      {
-        children: []
-      }
-    ],
     filename: null,
-    testObject: [
-      {
-        id: 1,
-        name: "repository",
-        children: []
-      }
-    ],
+    filecontent: null,
     loaded: true,
-    filecontent: null
+    ready: false,
+    repository: [
+      {
+        id: 0,
+        name: "root",
+        children: [{}]
+      }
+    ]
   }),
   created() {
     this.$vuetify.theme.dark = true;
   },
   methods: {
-    async getNextLayer(folder) {
+    async getNextRepositoryLayer(folder) {
       const graphqlWithAuth = graphql.defaults({
         headers: {
           authorization: `bearer  ${this.key1 + this.key2}`
         }
       });
       const { repository } = await graphqlWithAuth(`{
-              repository(owner: "onebadmuthajama", name: "hellovuejs") {
+              repository(owner: "kadenlovell", name: "hellovuejs") {
                 object(expression: "master:${folder}/") {
                   ... on Tree{
                         entries{
@@ -92,18 +74,16 @@ export default {
                 }
               }
             }`);
-      for (var i = 0; i < repository.object.entries.length; i++) {
-        this.subdir[0].children[i] = repository.object.entries[i];
-      }
+      console.log(repository);
     },
-    async getRepoFiles() {
+    async getRepositoryRoot() {
       const graphqlWithAuth = graphql.defaults({
         headers: {
           authorization: `bearer ${this.key1 + this.key2}`
         }
       });
       const { repository } = await graphqlWithAuth(`{
-          repository(owner: "onebadmuthajama", name: "hellovuejs") {
+          repository(owner: "kadenlovell", name: "hellovuejs") {
             object(expression: "master:") {
               ... on Tree{
                     entries{
@@ -113,88 +93,25 @@ export default {
             }
           }
         }`);
-
-      this.repository = repository;
+      console.log(repository.object.entries);
+      this.repository.children = [];
       for (var i = 0; i < repository.object.entries.length; i++) {
-        this.testObject[0].children[i] = repository.object.entries[i];
-        if (this.testObject[0].children[i].name.split(".").length == 1) {
-          await this.getStructure(this.testObject[0].children[i].name);
-        }
+        this.repository[0].children[i] = repository.object.entries[i];
       }
+      this.ready = true;
+      console.log(this.repository[0]);
     },
-
-    async loadFile(filename, parent) {
-      console.log(parent + "/" + filename);
-      const graphqlWithAuth = graphql.defaults({
-        headers: {
-          authorization: `bearer ${this.key1 + this.key2}`
-        }
-      });
-      if (parent === null || parent === "undefined") {
-        const { repository } = await graphqlWithAuth(`{
-      repository(owner: "onebadmuthajama", name: "hellovuejs") {
-        object(expression: "master:${filename}") {
-             ... on Blob {
-            text
-          }
-        }
-      }
-    }`);
-        if (filename.split(".").length != 1) {
-          if (repository.object != null) {
-            this.filename = filename;
-            this.filecontent = repository.object.text;
-          }
-        }
-      } else {
-        const { repository } = await graphqlWithAuth(`{
-          repository(owner: "onebadmuthajama", name: "hellovuejs") {
-            object(expression: "master:${parent}/${filename}") {
-              ... on Blob {
-                text
-              }
-            }
-          }
-        }`);
-        if (filename.split(".").length != 1) {
-          if (repository.object != null) {
-            this.filename = filename;
-            this.filecontent = repository.object.text;
-          }
-        }
-      }
-      // await this.getNextLayer(filename);
-      //await this.getStructure(filename);
-    },
-    async getStructure(filename) {
-      for (var i = 0; i < this.testObject[0].children.length; i++) {
-        if (this.testObject[0].children[i].name === filename) {
-          await this.getNextLayer(filename);
-          this.testObject[0].children[i].children = [];
-
-          for (var j = 0; j < this.subdir[0].children.length; j++) {
-            this.testObject[0].children[i].children[j] = {
-              name: `${this.subdir[0].children[j].name}`,
-              parent: `${this.testObject[0].children[i].name}`
-            };
-          }
-          console.log(this.testObject);
-        }
-      }
+    async itemClick(name) {
+      console.log(name);
     },
     async resetModelState() {
-      this.repository = null;
-      this.testObject = [
-        {
-          id: 1,
-          name: "repository",
-          children: []
-        }
-      ];
       this.filecontent = null;
       this.filename = null;
-      this.subdir = [
+      this.ready = false;
+      this.repository = [
         {
+          id: 0,
+          name: "root",
           children: []
         }
       ];
